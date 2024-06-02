8447773c5fc3be3aee3c37c332f5f859dae660d5
Pierre-Marie Allard <pierre-marie.allard@unifr.ch>
2024-06-02 13:17:58 +0200

    Update launcher.sh

--- src/launcher.sh
+++ src/launcher.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Define input and output directories
-input_dir="/media/share/mapp/public/QE_plus_unifr/raw"
-output_dir="/media/share/mapp/public/QE_plus_unifr/converted"
-log_file="/media/share/mapp/public/QE_plus_unifr/logfile.log"
-processed_files="/media/share/mapp/public/QE_plus_unifr/processed_files.txt"
+input_dir="/media/share/mapp/public/QE_plus_unifr/test/raw"
+output_dir="/media/share/mapp/public/QE_plus_unifr/test/converted"
+log_file="/media/share/mapp/public/QE_plus_unifr/test/logfile.log"
+processed_files="/media/share/mapp/public/QE_plus_unifr/test/processed_files.txt"
+lock_file="/media/share/mapp/public/QE_plus_unifr/test/convert_new_files.lock"
 
 # Create processed_files.txt if it doesn't exist
 if [ ! -f "$processed_files" ]; then
@@ -21,6 +22,18 @@ convert_file() {
     chambm/pwiz-skyline-i-agree-to-the-vendor-licenses wine msconvert "/data/$(basename "$file")" --outdir /output --mzML --64 --zlib
     echo "$(date): Finished converting file $file" >> "$log_file"
 }
+
+# Ensure only one instance of the script runs at a time
+if [ -e "$lock_file" ] && kill -0 $(cat "$lock_file"); then
+    echo "$(date): Script is already running." >> "$log_file"
+    exit
+fi
+
+# Create a lock file with the current PID
+echo $$ > "$lock_file"
+
+# Ensure lock file is removed on script exit
+trap "rm -f $lock_file" EXIT
 
 # Check for new .raw files and process them
 for file in "$input_dir"/*.raw; do
